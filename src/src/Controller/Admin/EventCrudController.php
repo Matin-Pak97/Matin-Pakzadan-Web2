49ea29965340552cd95cd3b85bb25f864624f5e8
MaT_PaK <matin.pakzad1997@gmail.com>
2022-07-06 21:56:26 +0430

    Assignment6 - Automated Test - EasyAdmin

---
+++ src/src/Controller/Admin/EventCrudController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Event;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class EventCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Event::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('organizer'),
+//            DateTimeField::new('startDateTme'),
+//            DateTimeField::new('endDateTime'),
+        ];
+    }
+}
